5d16f97583355946e8ec07b930dbc0341af9af52
kolvv8 <sarrin1212@naver.com>
2025-10-20 21:44:44 +0900

    feat(model): 커스텀 구분자 지정 함수 구현

--- src/main/java/calculator/model/StringToNumParser.java
+++ src/main/java/calculator/model/StringToNumParser.java
@@ -1,5 +1,27 @@
 package calculator.model;
 
+import java.util.Collections;
+import java.util.List;
+
 public class StringToNumParser {
     private static String DELIMITERS = ",:";
+    private static final String CUSTOM_DELIMITER_PREFIX = "//";
+    private static final String CUSTOM_DELIMITER_SUFFIX = "\\n";
+    private static final int CUSTOM_DELIMITER_PREFIX_LENGTH = CUSTOM_DELIMITER_PREFIX.length(); // 2
+
+    public void parse(String input) {
+        String numberString = input;
+        if (input.startsWith(CUSTOM_DELIMITER_PREFIX)) {
+            final int endDelimiterIndex = input.indexOf(CUSTOM_DELIMITER_SUFFIX);
+            final int beginNumberStringIndex = CUSTOM_DELIMITER_PREFIX_LENGTH + 1;
+
+            if (endDelimiterIndex == beginNumberStringIndex) {
+                String customDelimiter = input.substring(CUSTOM_DELIMITER_PREFIX_LENGTH, endDelimiterIndex);
+                numberString = input.substring(endDelimiterIndex + 2);
+
+                DELIMITERS += customDelimiter;
+            }
+        }
+        DELIMITERS = '[' + DELIMITERS + ']';
+    }
 }
