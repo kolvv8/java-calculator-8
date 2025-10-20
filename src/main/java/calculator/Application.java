50636e935977b66d616b90c8e4f8b6906c7e2fa5
kolvv8 <sarrin1212@naver.com>
2025-10-20 22:40:18 +0900

    feat(app): Application에 수행 로직 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,22 @@
 package calculator;
 
+import calculator.controller.CalculatorController;
+import calculator.model.StringCalculator;
+import calculator.model.StringToNumParser;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        StringToNumParser parser = new StringToNumParser();
+        StringCalculator stringCalculator = new StringCalculator(parser);
+
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        CalculatorController controller = new CalculatorController(stringCalculator, inputView, outputView);
+
+        controller.calculate();
     }
 }
